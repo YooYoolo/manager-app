7a2cf7fbc983d697be02a3f5322c2e9205b5847a
Yotsume <dreamsartem@gmail.com>
2025-11-13 00:07:48 +0300

    add ProductsController

---
+++ manager-app/src/main/java/com/yotsume/managerapp/controller/ProductsController.java
@@ -0,0 +1,40 @@
+package com.yotsume.managerapp.controller;
+
+import com.yotsume.managerapp.controller.payload.NewProductPayload;
+import com.yotsume.managerapp.entity.Product;
+import com.yotsume.managerapp.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.service.annotation.GetExchange;
+
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("catalogue/products")
+public class ProductsController {
+
+    private final ProductService productService;
+
+    @GetMapping(value = "list")
+    public String getProductsList(Model model) {
+        model.addAttribute("products", this.productService.findAllProducts());
+        return "catalogue/products/list";
+    }
+
+    @GetMapping("create")
+    public String getNewProductPage(Model model) {
+        return "catalogue/products/new_product";
+    }
+
+    @PostMapping("create")
+    public String CreateProduct(NewProductPayload payload){
+        Product product = this.productService.createProduct(payload.title(), payload.details());
+        return "redirect:/catalogue/products/list";
+
+    }
+}
