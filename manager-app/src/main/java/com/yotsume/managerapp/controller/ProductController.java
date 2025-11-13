e6ffcf920793205eb4ea01262f69f72e8336bbe1
Yotsume <dreamsartem@gmail.com>
2025-11-14 00:07:24 +0300

    add ExceptionHandler

--- manager-app/src/main/java/com/yotsume/managerapp/controller/ProductController.java
+++ manager-app/src/main/java/com/yotsume/managerapp/controller/ProductController.java
@@ -3,9 +3,18 @@ package com.yotsume.managerapp.controller;
 import com.yotsume.managerapp.controller.payload.UpdateProductPayload;
 import com.yotsume.managerapp.entity.Product;
 import com.yotsume.managerapp.service.ProductService;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
+import java.util.Locale;
+import java.util.NoSuchElementException;
 
 @Controller
 @RequiredArgsConstructor
@@ -14,9 +23,13 @@ public class ProductController {
 
     private final ProductService productService;
 
+    private final MessageSource messageSource;
+
     @ModelAttribute("product")
     public Product product(@PathVariable("productId") int productId){
-        return this.productService.findProduct(productId).orElseThrow();
+        return this.productService.findProduct(productId).orElseThrow(
+                () -> new NoSuchElementException("catalogue.errors.product.notFound")
+        );
     }
 
     @GetMapping()
@@ -31,15 +44,39 @@ public class ProductController {
 
     @PostMapping("edit")
     public String updateProduct(
-            @ModelAttribute("product") Product product,
-            UpdateProductPayload payload){
-        this.productService.updateProduct(product.getId(), payload.title(), payload.details());
-        return "redirect:/catalogue/products/%d".formatted(product.getId());
+            @ModelAttribute(value = "product", binding = false) Product product,
+            @Valid UpdateProductPayload payload,
+            BindingResult bindingResult,
+            Model model){
+
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("payload", payload);
+            model.addAttribute("errors",bindingResult.getAllErrors().stream()
+                    .map(ObjectError::getDefaultMessage).toList());
+            return "catalogue/products/edit";
+        } else {
+            this.productService.updateProduct(product.getId(), payload.title(), payload.details());
+            return "redirect:/catalogue/products/%d".formatted(product.getId());
+        }
     }
 
     @PostMapping("delete")
     public String DeleteProduct(@ModelAttribute("product") Product product) {
         this.productService.deleteProduct(product.getId());
         return "redirect:/catalogue/products/list";
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public String handleNoSuchElementException(NoSuchElementException e,
+                                               Model model,
+                                               HttpServletResponse response,
+                                               Locale locale) {
+        response.setStatus(HttpStatus.NOT_FOUND.value());
+        model.addAttribute("error",
+                this.messageSource.getMessage(e.getMessage(),
+                        new Object[0],
+                        e.getMessage(),
+                        locale));
+        return "errors/404";
     }
 }
--- manager-app/src/main/java/com/yotsume/managerapp/controller/ProductsController.java
+++ manager-app/src/main/java/com/yotsume/managerapp/controller/ProductsController.java
@@ -3,9 +3,12 @@ package com.yotsume.managerapp.controller;
 import com.yotsume.managerapp.controller.payload.NewProductPayload;
 import com.yotsume.managerapp.entity.Product;
 import com.yotsume.managerapp.service.ProductService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,15 +27,23 @@ public class ProductsController {
     }
 
     @GetMapping("create")
-    public String getNewProductPage(Model model) {
+    public String getNewProductPage() {
         return "catalogue/products/new_product";
     }
 
     @PostMapping("create")
-    public String CreateProduct(NewProductPayload payload){
-        Product product = this.productService.createProduct(payload.title(), payload.details());
-        return "redirect:/catalogue/products/%d".formatted(product.getId());
-
+    public String CreateProduct(@Valid NewProductPayload payload,
+                                BindingResult bindingResult,
+                                Model model){
+        if (bindingResult.hasErrors()){
+            model.addAttribute("payload", payload);
+            model.addAttribute("errors", bindingResult.getAllErrors().stream()
+                    .map(ObjectError::getDefaultMessage).toList());
+            return "catalogue/products/new_product";
+        } else {
+            Product product = this.productService.createProduct(payload.title(), payload.details());
+            return "redirect:/catalogue/products/%d".formatted(product.getId());
+        }
     }
 }
 
